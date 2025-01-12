ff45453b6b38f29c6054f6cd0794c73e4bdecf0c
Patryk Czarnik <patryk@patrykczarnik.net>
2025-01-12 16:59:46 +0100

    dodatkowe, co tam posiadam

---
+++ src/main/java/p35_daty/Formatowanie6_Parse.java
@@ -0,0 +1,19 @@
+package p35_daty;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Formatowanie6_Parse {
+
+	public static void main(String[] args) {
+		LocalDateTime data = LocalDateTime.now();
+
+		DateTimeFormatter df = DateTimeFormatter.ofPattern("EEEE,yyyyMMdd HH@mm");
+		System.out.println(data.format(df));
+		System.out.println(data);
+		System.out.println();
+		
+		data = LocalDateTime.parse("wtorek,19981208 12@13", df);
+		System.out.println(data);
+	}
+}
---
+++ src/main/java/p47_http/DownloadURL.java
@@ -0,0 +1,25 @@
+package p47_http;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+public class DownloadURL {
+
+	public static void main(String[] args) {
+		try {
+			URL url = new URL("http://api.nbp.pl/api/exchangerates/tables/A/");
+			try(InputStream wejscie = url.openStream()) {
+				Path wyjscie = Paths.get("kursy.json");
+				Files.copy(wejscie, wyjscie, StandardCopyOption.REPLACE_EXISTING);
+				System.out.println("Gotowe");
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
