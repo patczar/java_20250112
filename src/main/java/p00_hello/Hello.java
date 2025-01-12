17814a371cfe4ba0b5294424451e75cfa02b6e57
Patryk Czarnik <patryk@patrykczarnik.net>
2025-01-12 10:00:17 +0100

    Hello world

---
+++ src/main/java/p00_hello/Hello.java
@@ -0,0 +1,9 @@
+package p00_hello;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    // Ctrl+Shift+F10 - uruchom bieżący plik
+}
