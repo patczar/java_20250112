b324c4198e7b146944b765f95e66edf404820895
Patryk Czarnik <patryk@patrykczarnik.net>
2025-01-12 11:19:56 +0100

    Konto - początek

---
+++ src/main/java/p01_klasy/Konto.java
@@ -0,0 +1,35 @@
+package p01_klasy;
+
+public class Konto {
+    private final int numer;
+    private int saldo;
+    private Osoba wlasciciel;
+
+    public Konto(int numer, int saldo, Osoba wlasciciel) {
+        this.numer = numer;
+        this.saldo = saldo;
+        this.wlasciciel = wlasciciel;
+    }
+
+    @Override
+    public String toString() {
+        return "Konto{" +
+                "numer=" + numer +
+                ", saldo=" + saldo +
+                ", wlasciciel=" + wlasciciel +
+                '}';
+    }
+
+    public int getNumer() {
+        return numer;
+    }
+
+    public Osoba getWlasciciel() {
+        return wlasciciel;
+    }
+
+    public int getSaldo() {
+        return saldo;
+    }
+
+}
